cebca9e5b76bac5789c23175e02f0427ad3c71e1
Unevailable <105091134+Unevailable@users.noreply.github.com>
2022-05-07 23:08:21 -0400

    Update navbar.js

--- navbar/navbar.js
+++ navbar/navbar.js
@@ -1,3 +1,4 @@
+// This code helps with being able to use the hamburger menu
 $(document).on('click', '.header__burgerMenu__button, .header__layer', function() {
     $('header').toggleClass('menuOpen');
 });
